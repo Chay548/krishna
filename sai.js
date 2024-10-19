1f857b7e051df7c3cf4aaddc419870bf8bb71c3e
chayi chaithu <chaitanya>
2024-10-19 13:26:06 +0530

    vktt

--- sai.js
+++
@@ -1,9 +0,0 @@
-const exp=require('express');
-const ap=express();
-const port=3000;
-ap.get("/",(req,res)=>{
-    res.send("hk");
-});
-ap.listen(port,()=>{
-    console.log('pk is doing ${port}');
-});
